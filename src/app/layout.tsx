cd2889a26d7c00faa2c0371044f26d02af25abd6
Nicolas Moises <nicolas.nicc21@gmail.com>
2023-05-23 01:58:55 -0300

    fix. erros eslint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,27 @@
+import { ReactNode } from 'react'
 import './globals.css'
 import { Nunito_Sans, Familjen_Grotesk } from 'next/font/google'
 
-const nunito = Nunito_Sans({ subsets: ['latin']})
-const familjenGrotesk = Familjen_Grotesk({ subsets: ['latin'], variable: '--font-familjen-grotesk', weight: '700' })
+const nunito = Nunito_Sans({ subsets: ['latin'] })
+const familjenGrotesk = Familjen_Grotesk({
+  subsets: ['latin'],
+  variable: '--font-familjen-grotesk',
+  weight: '700',
+})
 
 export const metadata = {
   title: 'Flash Benefícios',
   description: 'Uma landing page criada com Nextjs, Tailwindcss e Typescript',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${nunito.className} ${familjenGrotesk.variable} bg-zinc-50 text-pink-800 font-sans`}>{children}</body>
+      <body
+        className={`${nunito.className} ${familjenGrotesk.variable} bg-zinc-50 text-pink-800 font-sans`}
+      >
+        {children}
+      </body>
     </html>
   )
 }
